e93f7dce5882cb57a2e82687cb3220d47542551e
Michael E. Fortunato <mef231@gmail.com>
2017-12-19 14:08:51 -0500

    fixed intential error for debugging purposes

--- examples/Si2/run.py
+++ examples/Si2/run.py
@@ -1,6 +1,6 @@
 from rewotes.kpointconvg import System, KPointConvg
 s = System()
-si = s.add_species(name='Si', mass=28.0855, potential='error')
+si = s.add_species(name='Si', mass=28.0855, potential='si_pbe_gbrv_1.0.upf')
 s.read_poscar('si.poscar')
 kconvg = KPointConvg(system=s)
 kdim = kconvg.find_convg(tol=1e-5)
