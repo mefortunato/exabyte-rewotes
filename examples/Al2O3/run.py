c1a06fa42e63892875fe0dec7a1b3c5ad40bbf6c
Michael E. Fortunato <mef231@gmail.com>
2017-12-20 17:00:07 -0500

    typos

--- examples/Al2O3/run.py
+++ examples/Al2O3/run.py
@@ -1,7 +1,7 @@
 from rewotes import crystal, qe, kpointconvg
 s = crystal.System()
-al = s.add_species(name='Al', mass=26.982, potential='')
-o = s.add_species(name='O', mass=15.999, potential='Al.blyp-hgh.UPF')
+al = s.add_species(name='Al', mass=26.982, potential='Al.blyp-hgh.UPF')
+o = s.add_species(name='O', mass=15.999, potential='O.blyp-hgh.UPF')
 s.read_poscar('mp-642363.poscar')
 calc = qe.Calculation(s)
 kconvg = kpointconvg.KPointConvg(calc)
